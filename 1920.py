854ac981c37301be24bb5ab5dc5751e0a94281de
Prastuti-Adhikari <adhikariprastuti50@gmail.com>
2026-01-04 00:44:39 +0545

    solved

---
+++ 1920.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buildArray(self, nums: list[int]) -> list[int]:
+        ans = []
+
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+
+        return ans
