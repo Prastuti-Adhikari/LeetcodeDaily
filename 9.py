0eaa95c1bf398ed37f49a4bff2ba8af591865542
Prastuti-Adhikari <adhikariprastuti50@gmail.com>
2026-01-07 13:10:25 +0545

    solved

---
+++ 9.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        reverse = x[::-1]
+        if reverse == x: 
+            return True
+        else:
+            return False
